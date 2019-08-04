b5a5f9e224e144ca5a0186846f38ec3d447cd33d
rupin <rupin.chheda@gmail.com>
2019-08-04 18:24:21 +0530

    Initial Generic Template

---
+++ project_name/admin.py
@@ -0,0 +1,18 @@
+from import_export.admin import ImportExportModelAdmin
+from django.contrib import admin
+from django.contrib.auth import get_user_model
+from django.contrib.auth.admin import UserAdmin
+
+from .forms import CustomUserCreationForm, CustomUserChangeForm
+from .models import *
+
+class CustomUserAdmin(UserAdmin):
+    add_form = CustomUserCreationForm
+    form = CustomUserChangeForm
+    model = CustomUser
+    list_display = ['email', 'username',]
+	
+
+
+admin.site.register(CustomUser, CustomUserAdmin)
+
